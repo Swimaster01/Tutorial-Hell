7c161ee1f36aff9ee717050165d945dc403782a6
Swimaster01 <152829444+Swimaster01@users.noreply.github.com>
2025-08-21 18:19:10 +0800

    Added code for practicing

---
+++ script.js
@@ -0,0 +1,62 @@
+// Chat Bot
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+
+let bot;
+let botLocation;
+
+bot = "teacherBot";
+botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+bot = "professorBot";
+
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
+
+const newNicknameGreeting =
+  "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+const favoriteSubject = "Computer Science";
+
+const favoriteSubjectSentence =
+  "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+
+console.log("Well, it was nice to talk to you. Have a nice day!");
+
+// Trivia bot
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Sparky";
+let botLocation = "havoc"; 
+let favoriteLanguage = 'HTML';
+
+console.log("My name is " + botName + " and I live on " + botLocation + '.';
+
+console.log('My favorite programming language is ' + favoriteLanguage + '.';
+
+let codingFact = 'I love to write ' + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "I love to read " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "I frick with " + favoriteLanguage;
+console.log(codingFact);
+
+console.log('it was fun sharing these facts with you.Goodbye! - ' + botName + ' from ' + botLocation + '.');
+
+// Sentence Maker
+
+
